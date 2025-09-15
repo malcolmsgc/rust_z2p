6d1f6a6de4089528c3d901781f3b2f226cc819c1
Malcolm Cumming <malcolmsgc@gmail.com>
2025-09-15 23:01:21 +0100

    ip and port printed to stdout on start up

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,10 @@ async fn health_check() -> HttpResponse {
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let addr = listener.local_addr()?;
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .listen(listener)?
         .run();
+    println!("HttpServer started on {}", addr);
     Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    println!("Zero2prod starting...");
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     run(listener)?.await
 }
