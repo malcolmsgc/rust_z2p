17f0d958178f0b6ceaa79584a5c2a2fc7f60e92c
Malcolm Cumming <malcolmsgc@gmail.com>
2025-09-18 17:37:33 +0100

    app configuration from yaml

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     println!("Zero2prod starting...");
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // panic if config can't be read
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
