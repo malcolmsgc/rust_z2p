c1d772a707b08d4f0c9ef99bebe1d0946cadfbce
Malcolm Cumming <malcolmsgc@gmail.com>
2025-09-18 14:46:45 +0100

    init db script allows skipping docker run

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -26,44 +26,50 @@ APP_USER="${APP_USER:=app}"
 APP_USER_PW="${APP_USER_PWD:=secret}"
 APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 
-# Launch postgres using Docker
-# Note, max connection number increased to 1000 for testing
-CONTAINER_NAME="postgres"
-docker run \
-    --env POSTGRES_USER=${SUPERUSER} \
-    --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
-    --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
-    --health-interval=1s \
-    --health-timeout=5s \
-    --health-retries=5 \
-    --publish "${DB_PORT}":5432 \
-    --detach \
-    --name "${CONTAINER_NAME}" \
-    postgres -N 1000
 
-# wait for postgres to be ready to accept connections
-# Walk-through:
-# - The `until ...; do ...; done` loop runs until the condition inside the brackets (`[ ... ]`) is true.
-# - `$(...)` is command substitution: it runs the command inside and replaces it with its output.
-# - The `-f` flag in `docker inspect -f` stands for “format”. It allows you to specify a Go template to format the output of `docker inspect`.
-until [ \
-    "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
-    "healthy" \
-]; do
-    >&2 echo "Postgres is still unavailable - sleeping"
-    sleep 1
-done
+# Skip Docker if dockerised PGSQL DB already running
+# - Note you can initialise a DB by setting SKIP_DOCKER to any value 
+#   (it is not a boolean evaluation, rather a string length check using -z):
+#       SKIP_DOCKER=anystring ./scripts/init_db.sh
+if [[ -z "${SKIP_DOCKER}" ]]; then
+    # Launch postgres using Docker
+    # Note, max connection number increased to 1000 for testing
+    CONTAINER_NAME="postgres"
+    docker run \
+        --env POSTGRES_USER=${SUPERUSER} \
+        --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+        --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+        --health-interval=1s \
+        --health-timeout=5s \
+        --health-retries=5 \
+        --publish "${DB_PORT}":5432 \
+        --detach \
+        --name "${CONTAINER_NAME}" \
+        postgres -N 1000
+
+
+    # Wait for postgres to be ready to accept connections
+    # - The `until ...; do ...; done` loop runs until the condition inside the brackets (`[ ... ]`) is true.
+    # - `$(...)` is command substitution: it runs the command inside and replaces it with its output.
+    # - The `-f` flag in `docker inspect -f` stands for “format”. It allows you to specify a Go template to format the output of `docker inspect`.
+    until [ \
+        "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+        "healthy" \
+    ]; do
+        >&2 echo "Postgres is still unavailable - sleeping"
+        sleep 1
+    done
+
+    # Create the application user
+    CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+    docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
+
+    # Grant create DB rights to app user
+    GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+    docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+fi
 
 echo "Postgres is up and running on port ${DB_PORT}!"
-
-# Create the application user
-CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
-docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
-
-# Grant create DB rights to app user
-GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
-docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
-
 echo "Running migrations now..."
 
 # Create the application database
