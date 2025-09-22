0e333d13b8b8e31c65f20799ec2f7cc5e5d7f38c
Malcolm Cumming <malcolmsgc@gmail.com>
2025-09-22 15:36:00 +0100

    connection to postgres added to app state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -7,7 +8,10 @@ async fn main() -> Result<(), std::io::Error> {
     println!("Zero2prod starting...");
     // panic if config can't be read
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,20 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // web::Data wraps our connection in an Atomic Reference Counted pointer (Arc)
+    let connection = web::Data::new(connection);
     let addr = listener.local_addr()?;
-    let server = HttpServer::new(|| {
+    // NB the `move` to capture `connection` in closure scope
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // pointer to PgConnection cloned and registered as part of actix_web App state
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
